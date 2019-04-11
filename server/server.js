447ffe3dcbd0bfaacac4a7ecfec10152fff426cc
nhishon <nhishon@luc.edu>
2019-04-10 21:20:00 -0500

    cors 2

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const session = require('express-session')
+const cors = require('cors');
 const dbConnection = require('./database')
 const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
 const app = express()
+app.use(cors());
 const PORT = process.env.PORT  || 5000;
-const cors = require('cors');
+
 
 
 
@@ -19,7 +21,7 @@ const router = express.Router();
 
 // Middleware
 app.use(morgan('dev'))
-app.use(cors());
+
 app.use(
     bodyParser.urlencoded({
         extended: false
@@ -43,6 +45,36 @@ app.use(passport.initialize())
 app.use(passport.session()) // calls the deserializeUser
 
 app.use('/user', user)
+
+app.all('*', function(req, res,next) {
+    /**
+     * Response settings
+     * @type {Object}
+     */
+    var responseSettings = {
+        "AccessControlAllowOrigin": req.headers.origin,
+        "AccessControlAllowHeaders": "Content-Type,X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name",
+        "AccessControlAllowMethods": "POST, GET, PUT, DELETE, OPTIONS",
+        "AccessControlAllowCredentials": true
+    };
+
+    /**
+     * Headers
+     */
+    res.header("Access-Control-Allow-Credentials", responseSettings.AccessControlAllowCredentials);
+    res.header("Access-Control-Allow-Origin",  responseSettings.AccessControlAllowOrigin);
+    res.header("Access-Control-Allow-Headers", (req.headers['access-control-request-headers']) ? req.headers['access-control-request-headers'] : "x-requested-with");
+    res.header("Access-Control-Allow-Methods", (req.headers['access-control-request-method']) ? req.headers['access-control-request-method'] : responseSettings.AccessControlAllowMethods);
+
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+
+
+});
 
 // Starting Server
 app.listen(PORT, () => {
