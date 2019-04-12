0138b175a7cbf125a7e29f9b3f057950e6eb4a65
nhishon <nhishon@luc.edu>
2019-04-12 08:35:08 -0500

    cors 6

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const MongoStore = require('connect-mongo')(session)
 const passport = require('./passport');
 const app = express()
 
-const PORT = process.env.PORT  || 5000;
+const PORT =  5000;
 
 
 
